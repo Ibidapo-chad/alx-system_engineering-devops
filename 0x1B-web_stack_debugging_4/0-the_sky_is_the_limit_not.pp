0e7c9fc89263cec91791f25528175da5c56c3134
Ibidapo-chad <Ibidapo.chad@gmail.com>
2021-10-08 02:53:20 +0000

    files added and updated

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+#use sed to update config files to allow nginx handle opening many files
+
+exec { 'fix--for-nginx':
+  path    => ['usr/local/sbin:usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => "sed -i '/error_log.*/a \\\topen_file_cache max=200000 inactive=20s;' /etc/nginx/nginx.conf &&\
+             sed -i '/error_log.*/a \\\topen_file_cache_valid 30s;' /etc/nginx/nginx.conf &&\
+             sed -i '/error_log.*/a \\\topen_file_cache_min_uses 2;' /etc/nginx/nginx.conf &&\
+             sed -i '/error_log.*/a \\\topen_file_cache_errors on;' /etc/nginx/nginx.conf",
+}
